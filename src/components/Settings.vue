eb302589562705789cf31e2b0eb1d77c07b4108f
loucass003 <loucass003@gmail>
2018-03-13 18:10:11 +0100

    Add current wireless

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -21,6 +21,12 @@
                 <template slot="items" slot-scope="props">
                   <td>{{ props.item.ssid || '(No SSID)' }}</td>
                   <td>{{ props.item.flags }}</td>
+                  <td>
+                    <v-btn icon @click.stop="{ dialog.open = true; dialog.item = props.item }">
+                      <v-icon v-html="props.item.connected ? 'delete' : 'add'"></v-icon>
+                    </v-btn>
+                    
+                  </td>
                 </template>
               </v-data-table>
             </v-card>
@@ -28,8 +34,24 @@
         </v-expansion-panel>
       </v-flex>
     </v-layout>
+    <v-dialog v-model="dialog.open" max-width="500px">
+      <v-card>
+        <v-card-title>
+          Wireless Network "{{ dialog.item.ssid }}"
+        </v-card-title>
+        <v-card-text>
+          <v-text-field 
+            v-model="dialog.pass"
+            label="Password"
+          ></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" flat @click.stop="chooseWireless()">Save</v-btn>
+          <v-btn color="primary" flat @click.stop="dialog.open = false">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
-
 </template>
 
 <script>
@@ -40,16 +62,23 @@
       return {
         networks_header: [
           { text: 'SSID', value: 'ssid'},
-          { text: 'FLAGS', value: 'flags'}
+          { text: 'FLAGS', value: 'flags'},
+          { text: 'Action', value: 'connected', width: "10px"}
         ],
         networksRequest: false,
-        networks: []
+        networks: [],
+        dialog: {
+          open: false,
+          item: {},
+          pass: ''
+        },
       }
     },
     sockets: {
       wifi_list({ data }) {
+        this.networks = data;  
         this.networksRequest = false;
-        this.networks = data;
+        console.log(this.networks);
       }
     },
     created() {
@@ -59,6 +88,9 @@
       refreshNetworks() {
         this.networksRequest = true;
         this.$socket.emit('refresh_networks')
+      },
+      chooseWireless() {
+        this.$socket.emit('choose_network', this.dialog.item.ssid, this.dialog.pass);
       }
     }
   }
